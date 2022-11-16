3c47060575b1f96a6453a7ea5afce9ad8c0af6ef
IfeanYiGodsgift <115725539+IfeanYiGodsgift@users.noreply.github.com>
2022-11-17 00:32:28 +0100

    Delete week3/practice_1/target directory

--- week3/practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\GIFT IMUSEH\Downloads\godsgift.csc101\week3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\GIFT IMUSEH\Downloads\godsgift.csc101\week3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
--- week3/practice_1/target/debug/practice_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\GIFT\ IMUSEH\Downloads\godsgift.csc101\week3\practice_1\target\debug\practice_1.exe: C:\Users\GIFT\ IMUSEH\Downloads\godsgift.csc101\week3\practice_1\src\main.rs
