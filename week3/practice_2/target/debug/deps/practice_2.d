2e8562a5c48fcfbd129643401a8cf217730864a8
IfeanYiGodsgift <115725539+IfeanYiGodsgift@users.noreply.github.com>
2022-11-17 00:34:23 +0100

    Delete week3/practice_2/target directory

--- week3/practice_2/target/debug/deps/practice_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\GIFT IMUSEH\Downloads\godsgift.csc101\week3\practice_2\target\debug\deps\practice_2.exe: src\main.rs
-
-C:\Users\GIFT IMUSEH\Downloads\godsgift.csc101\week3\practice_2\target\debug\deps\practice_2.d: src\main.rs
-
-src\main.rs:
