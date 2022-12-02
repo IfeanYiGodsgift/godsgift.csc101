44aa87c4483e01e1994dc4ece4224aacbbe079f4
IfeanYiGodsgift <godsgift.ifeanyi@pau.edu.ng>
2022-12-02 11:32:07 +0100

    (week7)

---
+++ week7/practice1/src/main.rs
@@ -0,0 +1,10 @@
+//property of GODSGIFT DAVID IFEANYI
+fn main() {
+    let v : Vec<i64> = Vec::new();
+
+    println!("\n The length of vec ::new is : {}",v.len() );
+
+    let v = vec! ["Grace","Effiong","Basil","Kareem","Susan"];
+
+    println!("\n The length of vec macro  is : {}",v.len() );
+}
